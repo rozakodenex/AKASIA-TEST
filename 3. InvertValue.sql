a92f83c461ec1b5139339b855563b8c76ffe834e
rozakodenex <112388635+rozakodenex@users.noreply.github.com>
2023-03-24 14:42:29 +0700

    Update 3. InvertValue.sql

--- 3. InvertValue.sql
+++ 3. InvertValue.sql
@@ -24,10 +24,14 @@ END
 
 /*** 
 
-This stored procedure accepts two input parameters, @Id and @Column, which are used to identify the rows to be updated and the column to be inverted, respectively. The procedure first checks if the @Column parameter is valid (i.e., one of 'A', 'B', 'C', 'D', or 'E'). If the parameter is not valid, the procedure raises an error and returns.
+This stored procedure accepts two input parameters, @Id and @Column, which are used to identify the rows to be updated and the column to be inverted, respectively. 
+The procedure first checks if the @Column parameter is valid (i.e., one of 'A', 'B', 'C', 'D', or 'E'). 
+If the parameter is not valid, the procedure raises an error and returns.
 
-The procedure then uses an UPDATE statement to update the specified column for the matching rows in the #TEST2 table. The CASE statement is used to invert the value of the column only if the existing value is not NULL. The tilde (~) operator is used to invert the value of the bit column.
+The procedure then uses an UPDATE statement to update the specified column for the matching rows in the #TEST2 table. 
+The CASE statement is used to invert the value of the column only if the existing value is not NULL. The tilde (~) operator is used to invert the value of the bit column.
 
-Note that this implementation assumes that the Id column in the #TEST2 table is a primary key, or at least has a unique index on it. If this is not the case, the UPDATE statement may update multiple rows, which may not be the desired behavior.
+Note that this implementation assumes that the Id column in the #TEST2 table is a primary key, or at least has a unique index on it. 
+If this is not the case, the UPDATE statement may update multiple rows, which may not be the desired behavior.
 
 ***/
